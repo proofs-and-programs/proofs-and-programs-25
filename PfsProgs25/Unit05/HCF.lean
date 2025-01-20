28222ea936262a84a088eb647b8a5d89011c4cae
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-20 15:37:50 +0000

    skeleton proofs

--- PfsProgs25/Unit05/HCF.lean
+++ PfsProgs25/Unit05/HCF.lean
@@ -4,3 +4,20 @@ import Mathlib
 
 As an example of proving termination, we define the function `hcf` that computes the greatest common divisor of two natural numbers. We prove that `hcf` terminates by showing that the argument decreases in each recursive call.
 -/
+def hcf (a b: Nat) : Nat :=
+  if p:b = 0 then a
+  else
+   if c:a < b then
+      hcf b a
+    else
+      have : a % b < a := by
+        apply lt_of_lt_of_le (b := b)
+        · apply Nat.mod_lt
+          simp [Nat.pos_iff_ne_zero]
+          assumption
+        · apply le_of_not_gt
+          assumption
+      hcf (a % b) b
+termination_by (b, a)
+
+#check lt_of_le_of_lt
--- PfsProgs25/Unit05/PowerIrrationals.lean
+++ PfsProgs25/Unit05/PowerIrrationals.lean
@@ -9,3 +9,21 @@ An interesting example of a non-constructive proof is the following:
 
 We can prove this in Lean. But a function that returns such a pair of numbers has to be defined as `noncomputable` in Lean.
 -/
+noncomputable abbrev sqrt_two := Real.sqrt 2
+
+theorem sq2_pow_twice : ((sqrt_two) ^ sqrt_two) ^ sqrt_two = 2 := by
+  rw [← Real.rpow_mul, Real.mul_self_sqrt]
+  · simp
+  · simp
+  · apply Real.sqrt_nonneg
+
+theorem exists_irrationals_pow_rational :
+  ∃ a b : ℝ, (Irrational a) ∧ (Irrational b) ∧ ¬ (Irrational (a ^ b)) := by
+    by_cases c:Irrational (sqrt_two ^ sqrt_two)
+    · use sqrt_two ^ sqrt_two, sqrt_two
+      simp [c]
+      apply And.intro
+      · apply irrational_sqrt_two
+      · simp [irrational_iff_ne_rational, sq2_pow_twice]
+    · use sqrt_two, sqrt_two
+      simp [irrational_sqrt_two, c]
